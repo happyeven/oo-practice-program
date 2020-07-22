b13e9701644257c45c334f2e56341c7e609a2575
happyeven <1361840595@qq.com>
2020-07-22 20:46:49 +0800

    [OO Step by Step practice 07] [Dong] feature:finish and pass the test

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,20 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + this.klass.getNumber() +".";
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,31 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    @Override
+    public String introduce() {
+        String teacherIntroduction = this.getKlass() == null ? "No Class." : "Class " + this.klass.getNumber() +".";
+        return super.introduce() + " I am a Teacher. I teach " + teacherIntroduction;
+    }
+    public String introduceWith(Student student){
+        String teacherIntroductionWith = this.getKlass().getNumber() == student.getKlass().getNumber() ? "" : " don't";
+        return getIntroductionTemplete(teacherIntroductionWith,student.getName());
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    public String getIntroductionTemplete(String teacherIntroductionWith,String studentName){
+        return super.introduce() + " I am a Teacher. I" + teacherIntroductionWith + " teach " + studentName + ".";
+    }
 }
