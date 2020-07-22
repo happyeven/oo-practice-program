3692802f860f06f187f63d947733de772ad7f2fc
happyeven <1361840595@qq.com>
2020-07-22 20:16:39 +0800

    [OO Step by Step practice 06] [Dong] feature:finish and pass the test

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+    public Teacher(String name,int age,int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    /*
+     * if use this constructor,
+     * will set the value of klass into -1
+     */
+    public Teacher(String name,int age){
+        super(name,age);
+        this.klass = -1;
+    }
+    @Override
+    public String introduce() {
+        String teacherIntroduction = this.getKlass() == -1 ? " I am a Teacher. I teach No Class." : " I am a Teacher. I teach Class " + this.klass +".";
+        return super.introduce() + teacherIntroduction;
+    }
+    public int getKlass() {
+        return klass;
+    }
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
