64432f8e6acf30508d5792ae0cbebd54c5db5b42
happyeven <1361840595@qq.com>
2020-07-22 23:44:26 +0800

    [OO Step by Step practice 11] [Dong] feature:create an module but not finish the task

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,46 @@
 package practice11;
 
+
+import java.util.ArrayList;
+
 public class Klass {
+    private int number;
+    private String displayName;
+    private Student leader;
+    private ArrayList<Student> studentList = new ArrayList<>();
+    public Klass(int number) {
+        this.number = number;
+        this.displayName = "Class " + number;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getDisplayName() {
+        return displayName;
+    }
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+    public Student getLeader() {
+        return leader;
+    }
+    private void setLeader(Student leader) {
+        this.leader = leader;
+    }
+    public void assignLeader(Student student){
+        if(this.isIn(student)) {
+            student.getKlass().setLeader(student);
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+    public void appendMember(Student student) {
+        studentList.add(student);
+    }
+    public boolean isIn(Student student) {
+        return studentList.contains(student);
+    }
 }
--- src/main/java/practice11/Person.java
+++ src/main/java/practice11/Person.java
@@ -1,4 +1,45 @@
 package practice11;
 
 public class Person {
+    protected String name;
+    protected int age;
+    protected int id;
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+    public Person(int id,String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null || this.getClass() != obj.getClass()){
+            return false;
+        }
+        Person person = (Person)obj;
+        return person.id == this.id;
+    }
 }
