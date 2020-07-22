73274489f846959ddaf781716a5a6fcdc384828a
DONG ZHU (ITA-ISDC-ISD-OOCLL/ZHA) <ZHUDO2@OOCL.COM>
2020-07-22 16:40:08 +0800

    [OO Step by Step] [Dong] [feature]:finish task 1

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,14 @@
 package practice02;
 
 public class Person {
+    protected String name;
+    protected int age;
+
+    protected Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void selfIntroduce(){
+        System.out.println("My name is " + this.name + ". I am "+ this.name + " years old.");
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,12 @@
 package practice02;
-
-public class Student {
+import practice02.Person;
+public class Student extends Person{
+    //add overvide
+    public Student(String name, int age){
+        super(name,age);
+    }
+    public String introduce(){
+        String introduceMessage = "My name is " + this.name + ". I am "+ this.name + " years old. Coding for the glory of OOCL.";
+        return introduceMessage;
+    }
 }
