16a458302d2be3e197d23c6488ac07647a09b06b
happyeven <1361840595@qq.com>
2020-07-22 21:28:49 +0800

    [OO Step by Step practice 08] [Dong] feature:finish and pass the test

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,32 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private String displayName;
+    private Student leader;
+    public Klass(int number) {
+        this.number = number;
+        this.displayName = "Class " + number;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getDisplayName() {
+        return displayName;
+    }
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+    public Student getLeader() {
+        return leader;
+    }
+    private void setLeader(Student leader) {
+        this.leader = leader;
+    }
+    public void assignLeader(Student student){
+        student.getKlass().setLeader(student);
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,48 @@
 package practice08;
 
+
 public class Person {
+    protected String name;
+    protected int age;
+    protected int id;
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+    public Person(int id,String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(this.getClass() != obj.getClass()){
+            return false;
+        }
+        Person person = (Person)obj;
+        return person.id == this.id;
+    }
 }
+
+
