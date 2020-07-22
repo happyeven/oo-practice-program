a9059475d2d0c25ca6f4361ce6d2c79d5ee47d34
happyeven <1361840595@qq.com>
2020-07-22 22:30:49 +0800

    [OO Step by Step practice 09] [Dong] feature:finish and pass the test

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,25 @@
 package practice09;
 
-public class Student {
+
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id,String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass = klass;
+    }
+    @Override
+    public String introduce() {
+        Student leader = this.klass.getLeader();
+        String leaderString = leader != null && leader.getName() == this.getName() ? "I am Leader of Class " +
+                this.getKlass().getNumber() + "." : "I am at Class " +
+                this.klass.getNumber() +".";
+        return super.introduce() + " I am a Student. " + leaderString;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
