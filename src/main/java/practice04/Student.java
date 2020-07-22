dbedbd83b51d85ff942a2f146eb62953a0f3a382
DONG ZHU (ITA-ISDC-ISD-OOCLL/ZHA) <ZHUDO2@OOCL.COM>
2020-07-22 17:21:30 +0800

    [OO Step by Step] [Dong] feature:finish practice04 and pass the test

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,23 @@
 package practice04;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age,int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    public Student(String name, int age) {
+        super(name,age);
+    }
+    @Override
+    public String introduce(){
+        return String.format("My name is %s. I am %d years old. I am a Student. I am at Class %d.", this.name, this.age,this.klass);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
